73afbb813e50821b76275797d101392f938d5961
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-02 22:39:29 +0800

    Differences between interface and abstract classes added

--- AbstractClasses/src/abstractclasses/CanFly.java
+++ AbstractClasses/src/abstractclasses/CanFly.java
@@ -2,4 +2,14 @@ package abstractclasses;
 
 public interface CanFly {
     void fly();
+    /*
+    Differences between abstract classes and interfaces
+    1. In abstract classes, methods can be private, protected or public. Methods in interfaces are public by default.
+    2. In abstract classes, member variables can be defined. Variables in interfaces can be defined but can only be
+    public static final, i.e. constant values with a static scope
+    as non-static variables require an instance, and an interface cannot be instantiated.
+    3. Abstract classes can have constructors, while interfaces cannot.
+    4. Abstract classes can have methods with an implementation, while interfaces cannot. All methods are to be defined
+    in the class implementing the interface.
+     */
 }
