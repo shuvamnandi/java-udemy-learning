ea515c867fa5ecc1a00e4b4af5db5ad3c7fb5af9
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-02 21:39:32 +0800

    Added Inner Classes code

--- InnerClasses/src/com/shuvamnandi/innerclasses/Gearbox.java
+++ InnerClasses/src/com/shuvamnandi/innerclasses/Gearbox.java
@@ -1,4 +1,31 @@
 package com.shuvamnandi.innerclasses;
 
+import java.util.ArrayList;
+
 public class Gearbox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    // Usually inner classes would be private and constructed only by outer class
+    public class Gear {
+        private int gearNumber = 0;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double driveSpeed(int revs){
+            return revs * this.ratio;
+        }
+    }
 }
--- InnerClasses/src/com/shuvamnandi/innerclasses/Main.java
+++ InnerClasses/src/com/shuvamnandi/innerclasses/Main.java
@@ -1,4 +1,17 @@
 package com.shuvamnandi.innerclasses;
 
 public class Main {
+    public static void main(String[] args){
+        Gearbox gearbox = new Gearbox(6);
+        Gearbox.Gear first = gearbox.new Gear(1, 12.2);
+        // Gearbox.Gear first = new Gearbox.Gear(1, 12.3); // works if Gear is a static inner class nested within Gearbox
+        // Gearbox.Gear second = new Gearbox.Gear(2, 15.3); // does not work
+        Gearbox.Gear second = gearbox.new Gear(2, 15.3);
+        // Gearbox.Gear third = new gearbox.Gear(3, 17.8); // does not work
+        Gearbox.Gear third = gearbox.new Gear(3, 17.8);
+
+        System.out.println("Speed: " + first.driveSpeed(1000));
+        System.out.println("Speed: " + second.driveSpeed(1000));
+        System.out.println("Speed: " + third.driveSpeed(1000));
+    }
 }
