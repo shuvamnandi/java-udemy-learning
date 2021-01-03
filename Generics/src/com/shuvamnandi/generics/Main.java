5c3d224799c391e8a437323d087bd25763f9dca6
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-03 13:26:27 +0800

    Modified List/ArrayList variable creation with usage of Generics

--- Generics/src/com/shuvamnandi/generics/Main.java
+++ Generics/src/com/shuvamnandi/generics/Main.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List items = new ArrayList();
+        List<Integer> items = new ArrayList<>();
         items.add(1);
         items.add(2);
         // error would come up in printDoubled method, which Java cannot detect until runtime
-        items.add("Tim");
+        //items.add("Tim");
         items.add(3);
         items.add(4);
         printDoubled(items);
     }
 
-    private static void printDoubled(List n){
-        for(Object i: n){
-            System.out.println((Integer)i*2);
+    private static void printDoubled(List<Integer> n){
+        for(int i: n) {
+            System.out.println(i*2);
         }
     }
 }
