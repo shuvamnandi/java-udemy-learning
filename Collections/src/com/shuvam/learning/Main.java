d73ff3a004816ef5b3ade5dec98dd7b6a15cd393
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-11 23:09:43 +0800

    Added exits to each location in Adventure Game

--- Collections/src/com/shuvam/learning/Main.java
+++ Collections/src/com/shuvam/learning/Main.java
@@ -58,6 +58,26 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("Q", 0);
+
         int loc = 1;
 
         while(true) {
@@ -65,8 +85,19 @@ public class Main {
             if (loc==0) {
                 break;
             }
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are: ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            }
+            else {
                 System.out.println("You cannot go in that direction");
             }
         }
