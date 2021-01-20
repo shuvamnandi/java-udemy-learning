b634ea379a8ecf335ec13e5e1b20eb8f601b5b32
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-21 00:16:42 +0800

    Added mor explanations

--- StaticKeyword/src/com/shuvamnandi/learning/Main.java
+++ StaticKeyword/src/com/shuvamnandi/learning/Main.java
@@ -15,10 +15,15 @@ public class Main {
         System.out.println("Number of instances: " + StaticTest.getNumInstances());
 
         /*
-        nUsually, class fields require an instance of the class that don't actually exist until an
+        Usually, class fields require an instance of the class that don't actually exist until an
         instance has been created. The main method is static and it can be called with the class instance.
         So as a result, Java can't allow a static method to access non-static fields or methods, because
         they don't exist when the static methods get called.
+        There's no problem with non-static methods accessing the static field.
+        We can also call static methods from non-static ones with no problems.
+        There's nothing to prevent a static method from accessing non-static fields and methods in another class
+        because it needs to create an instance of a class in order to do so.
+        The restriction is purely on a static method accessing non-static methods and fields in its own class.
          */
         int answer = multiply(6);
         System.out.println("The answer is: " + answer);
