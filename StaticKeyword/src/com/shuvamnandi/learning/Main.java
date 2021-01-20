22de5989cf1319b8391b718299a4af09920b2a41
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-21 00:08:13 +0800

    made getNumInstances a static method and now we can call it without an object of the class

--- StaticKeyword/src/com/shuvamnandi/learning/Main.java
+++ StaticKeyword/src/com/shuvamnandi/learning/Main.java
@@ -3,12 +3,12 @@ package com.shuvamnandi.learning;
 public class Main {
     public static void main(String[] args) {
         StaticTest firstInstance = new StaticTest("First instance");
-        System.out.println("Number of instances: " + firstInstance.getNumInstances());
+        System.out.println("Number of instances: " + StaticTest.getNumInstances());
 
         StaticTest secondInstance = new StaticTest("Second instance");
-        System.out.println("Number of instances: " + secondInstance.getNumInstances());
+        System.out.println("Number of instances: " + StaticTest.getNumInstances());
 
         StaticTest thirdInstance = new StaticTest("Third instance");
-        System.out.println("Number of instances: " + thirdInstance.getNumInstances());
+        System.out.println("Number of instances: " + StaticTest.getNumInstances());
     }
 }
--- StaticKeyword/src/com/shuvamnandi/learning/StaticTest.java
+++ StaticKeyword/src/com/shuvamnandi/learning/StaticTest.java
@@ -1,6 +1,11 @@
 package com.shuvamnandi.learning;
 
 public class StaticTest {
+    /*
+    A static field which is also known as a class variable is associated with the class rather than with any
+    particular instance of it, so it has only one copy of the variable in memory. All instances of the class
+    share that one class variable.
+     */
     private static int numInstances = 0;
     private String name;
 
@@ -9,7 +14,7 @@ public class StaticTest {
         numInstances++;
     }
 
-    public int getNumInstances() {
+    public static int getNumInstances() {
         return numInstances;
     }
 
