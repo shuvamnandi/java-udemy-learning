1dd7abce8af23940cbe26fc4dfcb826acddb2d7f
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-21 00:13:47 +0800

    Example of static method not allowed to use non-static methods/fields

--- StaticKeyword/src/com/shuvamnandi/learning/Main.java
+++ StaticKeyword/src/com/shuvamnandi/learning/Main.java
@@ -1,7 +1,10 @@
 package com.shuvamnandi.learning;
 
 public class Main {
+    public int multiplier = 7;
+
     public static void main(String[] args) {
+        System.out.println("Number of instances: " + StaticTest.getNumInstances());
         StaticTest firstInstance = new StaticTest("First instance");
         System.out.println("Number of instances: " + StaticTest.getNumInstances());
 
@@ -10,5 +13,19 @@ public class Main {
 
         StaticTest thirdInstance = new StaticTest("Third instance");
         System.out.println("Number of instances: " + StaticTest.getNumInstances());
+
+        /*
+        nUsually, class fields require an instance of the class that don't actually exist until an
+        instance has been created. The main method is static and it can be called with the class instance.
+        So as a result, Java can't allow a static method to access non-static fields or methods, because
+        they don't exist when the static methods get called.
+         */
+//        int answer = multiply(6);
+//        System.out.println("The answer is: " + answer);
+//        System.out.println("Multiplier is: " + multiplier);
+    }
+
+    public int multiply(int number) {
+        return number * multiplier;
     }
 }
--- StaticKeyword/src/com/shuvamnandi/learning/StaticTest.java
+++ StaticKeyword/src/com/shuvamnandi/learning/StaticTest.java
@@ -5,6 +5,7 @@ public class StaticTest {
     A static field which is also known as a class variable is associated with the class rather than with any
     particular instance of it, so it has only one copy of the variable in memory. All instances of the class
     share that one class variable.
+    A static method cannot access non-static variables, but an object can access static variables
      */
     private static int numInstances = 0;
     private String name;
@@ -15,10 +16,14 @@ public class StaticTest {
     }
 
     public static int getNumInstances() {
+        // A static method cannot access non-static variables, but an object can access static variables
+        // this keyword is not applicable inside a static context
+        // System.out.println("Printing the name: " + this.name);
         return numInstances;
     }
 
     public String getName() {
+        System.out.println("Printing the number of instances: "+numInstances);
         return name;
     }
 }
