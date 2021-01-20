50f438ca363982a59a53a4975479bed8922083aa
Shuvam Nandi <shuvamnandi9@gmail.com>
2021-01-20 22:57:29 +0800

    Added class for Scope as of lecture 146

---
+++ Scope/src/com/shuvam/learning/Main.java
@@ -0,0 +1,13 @@
+package com.shuvam.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String privateVar = "this is private to main()";
+        ScopeCheck scopeCheck = new ScopeCheck();
+        System.out.println("ScopeCheck.publicVar is: " + scopeCheck.publicVar);
+        System.out.println("ScopeCheck.privateVar is: " + scopeCheck.getPrivateVar());
+        // System.out.println("ScopeCheck.privateVar is: " + scopeCheck.privateVar); // this does not work
+        System.out.println("privateVar is: " + privateVar);
+    }
+}
---
+++ Scope/src/com/shuvam/learning/ScopeCheck.java
@@ -0,0 +1,22 @@
+package com.shuvam.learning;
+
+public class ScopeCheck {
+    public int publicVar = 0;
+    private int privateVar = 1;
+
+    public ScopeCheck() {
+        System.out.println("ScopeCheck created, publicVar = "+publicVar + ": privateVar = " + privateVar);
+    }
+
+    public int getPrivateVar() {
+        return privateVar;
+    }
+
+    public void timesTwo() {
+        int privateVar = 2;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " times 2 is " + i * privateVar);
+        }
+    }
+
+}
